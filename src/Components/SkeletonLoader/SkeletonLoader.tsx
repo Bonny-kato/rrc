e30b8c4261656aa45512913a460b073533090116
Black <86022898+Bonny-kato@users.noreply.github.com>
2023-12-08 00:26:39 +0300

    chore: improve code readability for `SkeletonLoader` component

--- src/Components/SkeletonLoader/SkeletonLoader.tsx
+++ src/Components/SkeletonLoader/SkeletonLoader.tsx
@@ -1,25 +1,22 @@
 import { FC, HTMLAttributes } from "react";
 import { twMerge } from "tailwind-merge";
 
+const baseStyledClasses = `
+    after:contents-['']
+    after:absolute 
+    after:inset-0 
+    after:-translate-x-full
+    after:bg-custom-gradient
+    after:animate-shimmer
+    relative overflow-hidden
+    bg-[#E7EDF0] rounded w-full py-2
+`;
+
 const SkeletonLoader: FC<HTMLAttributes<HTMLParagraphElement>> = ({
     className,
     ...rest
 }) => {
-    return (
-        <p
-            {...rest}
-            className={`
-                after:contents-['']
-                after:absolute 
-                after:inset-0 
-                after:-translate-x-full
-                after:bg-custom-gradient
-                after:animate-shimmer
-                relative overflow-hidden 
-               
-                ${twMerge("bg-[#E7EDF0]  rounded w-full py-2 ", className)}`}
-        />
-    );
+    return <p {...rest} className={twMerge(baseStyledClasses, className)} />;
 };
 
 export default SkeletonLoader;
