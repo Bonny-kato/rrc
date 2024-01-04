b4cc89a52bd555c257d6458b0e160a25458224e8
Black <86022898+Bonny-kato@users.noreply.github.com>
2024-01-04 23:55:05 +0300

    feat: upgrade `tailwindcss` and it's dependencies

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
-/** @type {import("tailwindcss").Config} */
-export default {
+import type { Config } from "tailwindcss";
+
+const config: Config = {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
         extend: {
@@ -51,3 +52,5 @@ export default {
         require("@tailwindcss/forms"),
     ],
 };
+
+export default config;
